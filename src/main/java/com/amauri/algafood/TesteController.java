62b464db4e785de20a7c02536f7061d424d10d97
Amauri Castro <amauri.p.castro@gmail.com>
2024-12-03 20:37:38 -0300

    Criado modelos de dominio Cozinha e Restaurante, adicionado registros no BD por meio de arquivo import.sql

--- src/main/java/com/amauri/algafood/TesteController.java
+++ src/main/java/com/amauri/algafood/TesteController.java
@@ -1,15 +1,37 @@
 package com.amauri.algafood;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ConfigurationProperties("notificador.email")
 public class TesteController {
+
+    private String hostServidor;
+
+    private String porta;
 
     @GetMapping("/ola")
     @ResponseBody
     public String teste() {
-        return "Olá Amauri novo testo";
+        return "O host é: " + hostServidor + " porta: " + porta;
+    }
+
+    public String getHostServidor() {
+        return hostServidor;
+    }
+
+    public void setHostServidor(String hostServidor) {
+        this.hostServidor = hostServidor;
+    }
+
+    public String getPorta() {
+        return porta;
+    }
+
+    public void setPorta(String porta) {
+        this.porta = porta;
     }
 }
