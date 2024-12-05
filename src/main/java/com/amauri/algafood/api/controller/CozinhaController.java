9d127c6f6a80bb94490bd9348ece103215775c98
Amauri Castro <amauri.p.castro@gmail.com>
2024-12-05 08:12:42 -0300

    Implementado o uso de Response Entity para respostas no endpoint buscar em CozinhasController

--- src/main/java/com/amauri/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/amauri/algafood/api/controller/CozinhaController.java
@@ -1,12 +1,14 @@
 package com.amauri.algafood.api.controller;
 
+import com.amauri.algafood.api.model.CozinhasXmlWrapper;
 import com.amauri.algafood.domain.model.Cozinha;
 import com.amauri.algafood.infrastructure.repository.CozinhaRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +22,16 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhasXmlWrapper listarXml() {
+        return new CozinhasXmlWrapper(cozinhaRepository.listar());
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
+        Cozinha cozinha = cozinhaRepository.buscar(id);
+        return cozinha != null ? ResponseEntity.ok(cozinha) : ResponseEntity.notFound().build();
     }
 }
