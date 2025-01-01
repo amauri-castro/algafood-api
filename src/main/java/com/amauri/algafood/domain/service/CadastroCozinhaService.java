090a4214b90f56159b73adef81d090c0ad6cd121
amauri-castro <amauri.p.castro@gmail.com>
2025-01-01 20:16:16 -0300

    alterado exception de entidade nao encontrada para extender de ResponseStatusException

--- src/main/java/com/amauri/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/amauri/algafood/domain/service/CadastroCozinhaService.java
@@ -7,7 +7,9 @@ import com.amauri.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class CadastroCozinhaService {
@@ -25,6 +27,7 @@ public class CadastroCozinhaService {
         } catch (EmptyResultDataAccessException e) {
             throw new EntidadeNaoEncontradaException(
                     String.format("Não existe um cadastro de Cozinha com o código %d",cozinhaId));
+
         } catch (DataIntegrityViolationException e) {
             throw new EntidadeEmUsoException(
                     String.format("Cozinha de código %d não pode ser removida, pois está em uso",cozinhaId));
