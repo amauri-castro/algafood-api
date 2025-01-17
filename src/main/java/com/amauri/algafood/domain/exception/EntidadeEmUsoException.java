b57cfc5bdcfadf468f49087ebaeebfa20cf981a8
amauri-castro <amauri.p.castro@gmail.com>
2025-01-16 22:34:42 -0300

    alterado apiExceptionHandler para extender de ResponseEntityExceptionHandler

--- src/main/java/com/amauri/algafood/domain/exception/EntidadeEmUsoException.java
+++ src/main/java/com/amauri/algafood/domain/exception/EntidadeEmUsoException.java
@@ -1,9 +1,5 @@
 package com.amauri.algafood.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)
 public class EntidadeEmUsoException extends NegocioException {
 
     public EntidadeEmUsoException(String mensagem) {
--- src/main/java/com/amauri/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/amauri/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,9 +1,5 @@
 package com.amauri.algafood.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entidade não encontrada")
 public abstract class EntidadeNaoEncontradaException extends NegocioException {
     public EntidadeNaoEncontradaException(String mensagem) {
         super(mensagem);
