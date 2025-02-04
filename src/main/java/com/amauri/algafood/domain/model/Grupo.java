75db6ea1968ddafd56ec26dc02296cbefebf9971
amauri-castro <amauri.p.castro@gmail.com>
2025-02-04 14:31:56 -0300

    commit para sincronizacao do projeto

--- src/main/java/com/amauri/algafood/domain/model/Grupo.java
+++ src/main/java/com/amauri/algafood/domain/model/Grupo.java
@@ -25,4 +25,5 @@ public class Grupo {
             joinColumns = @JoinColumn(name = "grupo_id"),
             inverseJoinColumns = @JoinColumn(name = "permissao_id"))
     private List<Permissao> permissoes = new ArrayList<>();
+
 }
