38aef1c3278ab3c990a9b3cc4f22bf8f456413eb
amauri-castro <amauri.p.castro@gmail.com>
2025-02-05 21:52:58 -0300

    Implementado regra de negócio para evitar usuários com e-mails duplicados

--- src/main/java/com/amauri/algafood/domain/repository/UsuarioRepository.java
+++ src/main/java/com/amauri/algafood/domain/repository/UsuarioRepository.java
@@ -2,5 +2,9 @@ package com.amauri.algafood.domain.repository;
 
 import com.amauri.algafood.domain.model.Usuario;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
 }
