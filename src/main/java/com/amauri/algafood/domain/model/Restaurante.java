60b73437ec3be1687e186840465fca64e348b3d6
amauri-castro <amauri.p.castro@gmail.com>
2025-02-07 08:16:28 -0300

    Implementado endpoints de abertura e fechamento de Restaurantes

--- src/main/java/com/amauri/algafood/domain/model/Restaurante.java
+++ src/main/java/com/amauri/algafood/domain/model/Restaurante.java
@@ -45,6 +45,8 @@ public class Restaurante {
 
     private Boolean ativo = Boolean.TRUE;
 
+    private Boolean aberto = Boolean.FALSE;
+
     @Column(nullable = false, columnDefinition = "datetime")
     @CreationTimestamp
     private OffsetDateTime dataCadastro;
@@ -77,5 +79,14 @@ public class Restaurante {
     public boolean adicionarFormaPagamento(FormaPagamento formaPagamento) {
         return getFormasPagamento().add(formaPagamento);
     }
+
+    public void abrir() {
+        setAberto(Boolean.TRUE);
+    }
+
+    public void fechar() {
+        setAberto(Boolean.FALSE);
+    }
+
 
 }
