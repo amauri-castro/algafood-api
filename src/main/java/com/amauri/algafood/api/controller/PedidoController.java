668e18f29cb4b83c07eb0d872711afeeeabd4d00
amauri-castro <amauri.p.castro@gmail.com>
2025-02-18 22:55:40 -0300

    Criado metodo listar com filtros usando Json filter para pedidos, comentado para service de exemplo futuro

--- src/main/java/com/amauri/algafood/api/controller/PedidoController.java
+++ src/main/java/com/amauri/algafood/api/controller/PedidoController.java
@@ -12,8 +12,12 @@ import com.amauri.algafood.domain.model.Pedido;
 import com.amauri.algafood.domain.model.Usuario;
 import com.amauri.algafood.domain.repository.PedidoRepository;
 import com.amauri.algafood.domain.service.EmissaoPedidoService;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -37,6 +41,23 @@ public class PedidoController {
 
     @Autowired
     private PedidoResumoModelAssembler pedidoResumoModelAssembler;
+
+//    @GetMapping
+//    public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
+//        List<Pedido> pedidos = pedidoRepository.findAll();
+//        List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(pedidos);
+//        MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
+//
+//        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+//        filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter
+//                .serializeAll());
+//        if(StringUtils.isNotBlank(campos)) {
+//            filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter
+//                    .filterOutAllExcept(campos.split(",")));
+//        }
+//        pedidosWrapper.setFilters(filterProvider);
+//        return pedidosWrapper;
+//    }
 
     @GetMapping
     public List<PedidoResumoModel> listar() {
