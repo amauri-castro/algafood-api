49073947dc57b1efd98288c436911b0fd3e58456
amauri-castro <amauri.p.castro@gmail.com>
2025-02-27 13:33:22 -0300

    Implementado endpoint para atualizacao de foto de produtos com upload de arquivos usando multipartfile

---
+++ src/main/java/com/amauri/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,41 @@
+package com.amauri.algafood.api.controller;
+
+import com.amauri.algafood.api.model.input.FotoProdutoInput;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+                              @RequestParam MultipartFile arquivo, FotoProdutoInput fotoProdutoInput) {
+
+        var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+
+        Path diretorioBase = Paths.get(System.getProperty("user.home"), "downloads", "uploads-backend");
+
+        var arquivoFoto = Path.of(diretorioBase.toString(), nomeArquivo);
+
+
+        System.out.println(fotoProdutoInput.getDescricao());
+        System.out.println(arquivoFoto);
+        System.out.println(fotoProdutoInput.getArquivo().getContentType());
+
+        try {
+            arquivo.transferTo(arquivoFoto);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+}
