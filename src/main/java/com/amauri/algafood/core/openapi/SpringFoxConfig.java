763986cd27f1f7eed449f6a5b7da72f23b0957a6
amauri-castro <amauri.p.castro@gmail.com>
2025-03-10 08:48:22 -0300

    Implementado represetacao de codigos de erro na documentação da api

--- src/main/java/com/amauri/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/amauri/algafood/core/openapi/SpringFoxConfig.java
@@ -1,28 +1,33 @@
 package com.amauri.algafood.core.openapi;
 
+import com.amauri.algafood.api.exceptionhandler.Problem;
+import com.fasterxml.classmate.TypeResolver;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.builders.*;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
+
+    TypeResolver typeResolver = new TypeResolver();
 
     @Bean
     public Docket apiDocket() {
@@ -37,16 +42,21 @@ public class SpringFoxConfig {
                 .globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
                 .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
                 .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+                .additionalModels(typeResolver.resolve(Problem.class))
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"));
 
     }
+
+
 
     private List<Response> globalGetResponseMessages() {
         return Arrays.asList(
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                         .description("Erro interno do servidor")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
@@ -61,10 +71,14 @@ public class SpringFoxConfig {
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
                         .description("Requisição inválida")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                         .description("Erro interno no servidor")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
@@ -74,6 +88,8 @@ public class SpringFoxConfig {
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
                         .description("Requisição recusada porque o corpo está em um formato não suportado")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build()
         );
     }
@@ -83,12 +99,22 @@ public class SpringFoxConfig {
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
                         .description("Requisição inválida (erro do cliente)")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build(),
                 new ResponseBuilder()
                         .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                         .description("Erro interno no servidor")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(getProblemaModelReference())
                         .build()
         );
+    }
+
+    private Consumer<RepresentationBuilder> getProblemaModelReference() {
+        return r -> r.model(m -> m.name("Problema")
+                .referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
+                        q -> q.name("Problema").namespace("com.amauri.algafood.api.exceptionhandler")))));
     }
 
 
@@ -99,5 +125,10 @@ public class SpringFoxConfig {
                 .version("1.0.0")
                 .contact(new Contact("Amauri Dev", "https://www.amauri-dev.com.br", "amauri.p.castro@gmail.com"))
                 .build();
+    }
+
+    @Bean
+    public JacksonModuleRegistrar springFoxJacksonConfig() {
+        return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
     }
 }
