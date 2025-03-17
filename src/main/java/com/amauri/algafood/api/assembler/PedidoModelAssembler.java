2bdceb94931706f27112e5ac6607b840ed8453a6
amauri-castro <amauri.p.castro@gmail.com>
2025-03-17 07:45:17 -0300

    Adicionado template variables no link para recurso de pedidos no metodo toModel em PedidoModelAssembler

--- src/main/java/com/amauri/algafood/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/amauri/algafood/api/assembler/PedidoModelAssembler.java
@@ -5,6 +5,10 @@ import com.amauri.algafood.api.model.PedidoModel;
 import com.amauri.algafood.domain.model.Pedido;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
@@ -26,7 +30,19 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
         PedidoModel pedidoModel = createModelWithId(pedido.getId(), pedido);
         modelMapper.map(pedido, pedidoModel);
 
-        pedidoModel.add(linkTo(PedidoController.class).withRel("pedidos"));
+        TemplateVariables pageVariables = new TemplateVariables(
+                new TemplateVariable("page", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("size", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("sort", TemplateVariable.VariableType.REQUEST_PARAM)
+
+        );
+
+        String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
+
+        pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl,
+                pageVariables), "pedidos"));
+
+//        pedidoModel.add(linkTo(PedidoController.class).withRel("pedidos"));
 
         pedidoModel.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
                 .buscar(pedidoModel.getRestaurante().getId())).withSelfRel());
