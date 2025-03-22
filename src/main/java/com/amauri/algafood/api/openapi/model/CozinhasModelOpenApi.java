635e7b6ba29625d258bf4a1499ad49de6753ac5b
amauri-castro <amauri.p.castro@gmail.com>
2025-03-22 15:00:53 -0300

    Correção na paginação na documentação

--- src/main/java/com/amauri/algafood/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/com/amauri/algafood/api/openapi/model/CozinhasModelOpenApi.java
@@ -2,8 +2,27 @@ package com.amauri.algafood.api.openapi.model;
 
 import com.amauri.algafood.api.model.CozinhaModel;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.Links;
+
+import java.util.List;
 
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi extends PagedModelOpenApi<CozinhaModel>{
+@Getter
+@Setter
+public class CozinhasModelOpenApi {
 
+    private CozinhasEmbeddedModelOpenApi _embedded;
+    private Links _links;
+    private PageModelOpenApi page;
+
+    @ApiModel("CozinhasEmbeddedModel")
+    @Data
+    public class CozinhasEmbeddedModelOpenApi {
+
+        private List<CozinhaModel> cozinhas;
+
+    }
 }
