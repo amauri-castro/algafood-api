bbac7a020fa892a4c74379ea0ef7a5b70ba726b0
amauri-castro <amauri.p.castro@gmail.com>
2025-05-13 22:59:28 -0300

    Implementado fluxo de authorization code

--- src/main/java/com/amauri/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/amauri/algafood/core/security/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.amauri.algafood.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,8 +22,8 @@ public class ResourceServerConfig {
                 .and()
                 .csrf().disable()
                 .cors().and()
-                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer().jwt();
 
-        return http.build();
+        return http.formLogin(Customizer.withDefaults()).build();
     }
 }
